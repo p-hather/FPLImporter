74e58bf1e0e40edcd4d410ed33b538f5908bff72
Phil Hather <philhather@gmail.com>
2023-03-07 22:56:38 +0000

    refactor config/endpoints logic

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import yaml
 import requests
-import os
 import logging
 from dotenv import load_dotenv
-from python import bigquery
+from python.bigquery import bigQueryLoad
 
 
 logging.basicConfig(level=logging.INFO, filename="FPLImporter.log", filemode="a+",
@@ -14,25 +13,31 @@ load_dotenv(env_path)
 
 
 class FPLImporter:
-    def __init__(self, bq_project, bq_dataset, endpoint_fp='endpoints.yaml') -> None:
+    def __init__(self, config_fp='config.yaml', endpoints_fp='endpoints.yaml') -> None:
         self.base_url = 'https://fantasy.premierleague.com/api'
-        self.endpoint_fp = endpoint_fp
-        self.endpoints = self.get_endpoints(self.endpoint_fp)
+        self.config = self.read_yaml(config_fp)
+        self.bq = bigQueryLoad(self.config["bigquery"]["project"], self.config["bigquery"]["dataset"])
+        self.endpoints = self.read_yaml(endpoints_fp)
 
-        self.bq_project = bq_project
-        self.bq_dataset = bq_dataset
-        self.bq = bigquery.bigQueryLoad(self.bq_project, self.bq_dataset)
-
-    def get_endpoints(self, fp) -> dict:
-        with open(fp) as endpoints_file:
-            return yaml.safe_load(endpoints_file)
+    def read_yaml(self, fp) -> dict:
+        with open(fp) as file:
+            return yaml.safe_load(file)
     
-    def import_data(self) -> None:
-        for endpoint in self.endpoints:
-            url = f'{self.base_url}/{endpoint["path"]}/'
-            data = requests.get(url).json()
+    def get_data(self, path, type) -> list:
+        url = f'{self.base_url}/{path}/'
+        if type == 'team':
+            data = []
+            for team_id in self.config["fpl"]["team_ids"]:
+                team_data = requests.get(url.replace('%TEAM_ID%', str(team_id))).json()
+                team_data["team_id"] = team_id
+                data.append(team_data)
+            return data
+        return requests.get(url).json()
 
-            table_id = self.bq.create_table_id(endpoint["name"])
+    def import_data(self) -> None:
+        for name, endpoint in self.endpoints.items():
+            data = self.get_data(endpoint["path"], endpoint["type"])
+            table_id = self.bq.create_table_id(name)
             self.bq.load_table(data, table_id, description=endpoint["description"])
     
     def run_process(self) -> None:
@@ -43,7 +48,5 @@ class FPLImporter:
 
 
 if __name__ == '__main__':
-    bq_project = os.getenv('BQ_PROJECT')
-    bq_dataset = 'fpl'
-    fpl = FPLImporter(bq_project, bq_dataset)
+    fpl = FPLImporter()
     fpl.run_process()
